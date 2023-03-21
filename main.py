8c8d4d7078e21f5d5f669d8366e340500bcaa3ae
drive-expert <drive.expert35@gmail.com>
2023-03-21 16:59:47 +0300

    work

--- main.py
+++ main.py
@@ -8,3 +8,26 @@ def send_message(group_id,message_text):
     url = f'https://api.vk.com/method/messages.send?v=5.131&access_token={kate_token}&peer_id=-{group_id}&random_id=0&message={message_text}'
     result = requests.get(url)
     print(result.content)
+
+def create_lead_espo():
+    api = 'ade52dcd78e39b1b096699bdda06326a'
+    header = {
+        'X-Api-Key': 'e6bb529c5f80f8fc799aed7e6e9dcbdb',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    url = 'https://евроснаб.драйвэксперт.рф/api/v1/LeadCapture/'
+    url_c = f'{url}{api}'
+    data = {
+        "firstName": "Test Name",
+        "lastName": "Test Lead",
+        "emailAddress:": "test@test.test",
+        "vkURL": "https://vk.com/test",
+        "createdAccount": "TEST TEST TEST",
+        "website": "test.ru"
+    }
+    res = requests.post(url_c, data=data)
+
+    print(res.content)
+
+create_lead_espo()
